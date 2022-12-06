898f0228a80e8a52e7afe0c5247b9aaff2e51114
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-12-06 15:42:12 +0300

    started to build out selectors, constants, and drawing functionality

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # js-gajc7k
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-gajc7k)
+
+Canvas and context (2d or 3d) connect to a set of apis and methods that are used for drawing to the canvas.
+
+Think Microsoft paint - how it allows you to drag out shapes, change color, etc.
+
+beginPath(); is like putting the marker down on paper, it starts the drawing. Look it up on MDN Docs
+
+To randomize where the drawing starts on our etch canvas, we randomize a number between the width and height of the drawing area.
--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascript code!
+//select the elements on the page, canvas shake button
+const canvas = document.querySelector('#etch-a-sketch');
+const ctx = canvas.getContext('2d'); // ctx short for for context
+const shakebutton = document.querySelector('.shake');
 
+//setup our canvas for drawing
+
+const {width, height } = canvas; // a var called height and width from the same props on our canvas
+
+// create a radom x and y starting points for drawing
+
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 15;
+
+ctx.beginPath(); // drawing starts
+ctx.moveTo(200, 200);
+ctx.lineTo(200, 200);
+ctx.stroke();
+
+
+// write a draw function
+
+
+//write a handler for the keys
+
+
+//clear with shake button
+
+
+
+// listen for arrow keys
