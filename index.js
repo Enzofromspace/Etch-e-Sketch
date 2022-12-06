8f835a68c48631df479b202be7bb4518a0552287
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-12-06 16:02:44 +0300

    Got console to register keyboard arrows

--- index.js
+++ index.js
@@ -10,26 +10,38 @@ const shakebutton = document.querySelector('.shake');
 
 const {width, height } = canvas; // a var called height and width from the same props on our canvas
 
+
 // create a radom x and y starting points for drawing
+let x = Math.floor(Math.random() * width); 
+let y = Math.floor(Math.random() * height); 
 
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 15;
 
 ctx.beginPath(); // drawing starts
-ctx.moveTo(200, 200);
-ctx.lineTo(200, 200);
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
 ctx.stroke();
 
 
 // write a draw function
-
+function draw(options) {
+  console.log(options);
+}
 
 //write a handler for the keys
-
+function handleKey(e) {
+  if (e.key.includes('Arrow')){
+    e.preventDefault();
+    draw({key: e.key});
+    console.log(e.key);
+    console.log('handled');
+  }
+}
 
 //clear with shake button
-
-
+window.addEventListener('keydown', handleKey);
+console.log(handleKey);
 
 // listen for arrow keys
