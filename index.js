5fb747123f8ad1bbab1d2835e1e23921174f15fa
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-12-06 14:53:04 +0300

    1 file modified

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>An Etch 2 Sketch</h1>`;
+appDiv.innerHTML = `<h1>An Etch 2 Sketch</h1>`; 
